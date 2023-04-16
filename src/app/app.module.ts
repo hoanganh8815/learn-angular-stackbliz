9a57f30f883b0e6ea0ba1d475c2c571bcf2dc1b6
hoanganh8815 <nhanh36@gmail.com>
2023-04-16 14:52:53 +0700

    5 files modified, 3 added, 5 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { ServerComponent } from './server/server.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ServerComponent ],
+  declarations: [ AppComponent, SideBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
